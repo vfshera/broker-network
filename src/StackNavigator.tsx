f130b2d410d2a7064968a69488ce5ea7f75d9a7a
Franklin Shera <fshera96@gmail.com>
2022-03-03 01:13:07 +0300

    Initial Onboarding

--- src/StackNavigator.tsx
+++ src/StackNavigator.tsx
@@ -2,14 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import IntroScreen from "./screens/IntroScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Intro"
       screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name="Intro" component={IntroScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ExamleComponent from "../components/ExamleComponent";
-import timeService from "../services/TimeService";
 
 const HomeScreen = () => {
-  console.log("Time", timeService());
-
   return (
     <View>
       <Text>HomeScreen</Text>
