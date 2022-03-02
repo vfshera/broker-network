ce6560d5977f9ea8c8fe780fadfa563399894558
Franklin Shera <fshera96@gmail.com>
2022-03-02 23:52:01 +0300

    Abolute Path Alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "babel-plugin-root-import",
+        {
+          rootPathPrefix: "@app",
+          rootPathSuffix: "src",
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: [
+          "babel-plugin-root-import",
+          {
+            rootPathPrefix: "@app",
+            rootPathSuffix: "src",
+          },
+        ],
+      },
+    },
   };
 };
