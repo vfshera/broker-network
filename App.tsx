6fe9a6866e581922876dec634c3b7e25a10d92e6
Franklin Shera <fshera96@gmail.com>
2022-03-03 01:11:16 +0300

    Custom Fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/StackNavigator";
+import { useFonts, Poppins_500Medium } from "@expo-google-fonts/poppins";
+import Loader from "./src/components/Loader";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Poppins_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <Loader />;
+  }
+
   return (
     <NavigationContainer>
       <StackNavigator />
