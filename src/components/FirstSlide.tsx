bb658eda811f0cdc79c9ad867f2720d081dc8d2d
Franklin Shera <fshera96@gmail.com>
2022-03-03 05:02:10 +0300

    First Slide Page

---
+++ src/components/FirstSlide.tsx
@@ -0,0 +1,89 @@
+import {
+  Dimensions,
+  ImageBackground,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+
+const { width, height } = Dimensions.get("window");
+const FirstSlide = () => {
+  return (
+    <ImageBackground
+      source={require(`@app/assets/images/one.webp`)}
+      resizeMode="cover"
+      style={styles.imgBG}
+    >
+      <SafeAreaView style={styles.overlay}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.logo}>.homs</Text>
+        </View>
+
+        <View>
+          <View style={styles.headingContainer}>
+            <Text style={styles.heading}>Find</Text>
+            <Text style={styles.heading}>the perfect</Text>
+            <Text style={styles.heading}>place</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.info}>Post your requirements</Text>
+            <Text style={styles.info}>and get highly relevant matches.</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+export default FirstSlide;
+
+const styles = StyleSheet.create({
+  headingContainer: {
+    width: "100%",
+    paddingHorizontal: 30,
+  },
+  infoContainer: {
+    width: "100%",
+    paddingHorizontal: 30,
+    paddingTop: 50,
+    marginBottom: 150,
+  },
+  info: {
+    color: "white",
+    fontSize: 15,
+    fontFamily: "Poppins_500Medium",
+    lineHeight: 20,
+  },
+  heading: {
+    color: "white",
+    fontSize: 50,
+    fontFamily: "Poppins_600SemiBold",
+    lineHeight: 60,
+  },
+  logoContainer: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 5 : 0,
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+  },
+  logo: {
+    color: "white",
+    fontSize: 38,
+    fontFamily: "Poppins_600SemiBold",
+  },
+  imgBG: {
+    flex: 1,
+    position: "relative",
+    width: width,
+  },
+  overlay: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "space-between",
+    backgroundColor: "#11111122",
+  },
+});
